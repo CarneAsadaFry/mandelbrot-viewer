397c46d296628dcbd00709ad857b06bf317c7fca
CarneAsadaFry <ntausik@berkeley.edu>
2021-01-02 18:03:11 -0800

    Update for IntelliJ

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module mandelbrot.viewer {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires java.desktop;
+
+    exports application;
+    opens application to javafx.graphics;
+}
